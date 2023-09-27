dcc59221f9539a632cbc9f75ff7051478b9805b0
Moab <guilhermeferrazbonato@gmail.com>
2023-09-27 10:37:33 -0300

    express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 import dotenv from 'dotenv'
+import  express  from 'express'
+
 dotenv.config() 
+
+const app = express()
+const porta = process.env.API_PORT ?? 4000
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.listen(porta, () =>{
+    console.log(`🔥 Servidor Rodando na porta:${porta}! 🔥`)
+})
